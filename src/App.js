cd5274ce8ec93e0ee92539cdf9c5057d3f1f744f
Ahmed Chouihi <ahmedchouihi@gmail.com>
2024-03-10 01:18:13 +0100

    AI Code Buddy Start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const App=()=> {
+  return (
+    <div className="App">
+      <section className="search-section">
+        <p>What do you want to know?
+        <button className="surprise">Surprise me !</button>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
